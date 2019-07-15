28bcda40f94a9e8a8091be0c2f5ba9988722f147
tiffanybacher <tiffanybacher@gmail.com>
2019-07-15 16:25:46 -0600

    Fix failing App test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>CODERDOJO!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
