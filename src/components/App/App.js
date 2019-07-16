3c065a1695549b8d1059431411ba019de6fec4a6
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-15 21:08:39 -0600

    Update App component to test apollo fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import queries from '../../utils/queries/queries';
 
 export class App extends Component {
+  state = {
+    error: '',
+    users: {}
+  }
+
+  async componentDidMount() {
+    try {
+      const users = await queries.getUsers();
+      this.setState({ users })
+    } catch({ message }) {
+      this.setState({ error: message })
+    }
+  }
+
   render() {
     return (
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -8,24 +8,15 @@ import { rootReducer } from './reducers';
 import thunk from 'redux-thunk';
 import App from './components/App/App';
 import './index.scss';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-
-// apollo client setup
-const client = new ApolloClient({
-  uri: 'https://pure-castle-14648.herokuapp.com/graphql'
-})
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 const router = (
-  <ApolloProvider client={ client }>
     <Provider store={store}>
       <BrowserRouter>
         < App />
       </BrowserRouter>
     </Provider>
-  </ApolloProvider>
 )
 
 
