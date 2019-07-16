1f95a07d45c9c31357685b93cffdfe5c980fd1bd
Erik Konner <erikkonner@gmail.com>
2019-07-16 16:14:10 -0600

    Created routes for known routes

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import LoginForm from '../LoginForm/LoginForm';
+import Profile from '../Profile/Profile';
+import CurrentStudents from '../CurrentStudents/CurrentStudents';
+import ContactInfo from '../ContactInfo/ContactInfo';
+import Family from '../Family/Family';
 
 function Controls() {
   return (
     <div>
-
+      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/student/:id" component={Profile} />
+      <Route exact path="/students" component={CurrentStudents} />
+      <Route exact path="/contact/:id" component={ContactInfo} />
+      <Route exact path="myFamily" component={Family} />
     </div>
   );
 }
