9d588ab0a7fad413335d4306c676d3fb15f9c32f
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-15 19:50:26 -0600

    Wrap App component with apollo provider, allows access to graphql

--- src/index.js
+++ src/index.js
@@ -7,16 +7,25 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './reducers';
 import thunk from 'redux-thunk';
 import App from './components/App/App';
-import './index.scss'
+import './index.scss';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'https://pure-castle-14648.herokuapp.com/graphql'
+})
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 const router = (
-  <Provider store={store}>
-    <BrowserRouter>
-      < App />
-    </BrowserRouter>
-  </Provider>
+  <ApolloProvider client={ client }>
+    <Provider store={store}>
+      <BrowserRouter>
+        < App />
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>
 )
 
 
