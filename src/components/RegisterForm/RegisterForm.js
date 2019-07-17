6312451c61a65201dacf5aa4fba9c61ac23a5986
Erik Konner <erikkonner@gmail.com>
2019-07-17 12:41:40 -0600

    Added local state properties, created form w/ username, fullname, and password inputs

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -3,15 +3,36 @@ import { Proptypes } from 'prop-types';
 import { connect } from 'react-redux';
 
 export class RegisterForm extends Component {
-  state ={
-
+  state = {
+    username: '',
+    password:'',
+    reEnteredPassword: '',
+    fullName:''
   }
 
   render() {
     return (
-      <div>
-        
-      </div>
+      <form>
+        <h2>Create Account</h2>
+        <label htmlFor="full-name-input">
+          Full Name
+          <input 
+            id="full-name-input"
+            type="text" 
+            name="fullName"
+            onChange={this.handleChange}
+          />
+        </label>
+        <label htmlFor="username-input">
+          <input 
+            id="username-input"
+            type="text"
+            name="username"
+            onChange={this.handleChange}
+          />
+        </label>
+
+      </form>
     );
   }
 }
