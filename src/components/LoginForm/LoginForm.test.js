11103a8dd0753c4b88893b8ba668d87f0e1e0a66
tiffanybacher <tiffanybacher@gmail.com>
2019-07-16 21:17:48 -0600

    Write testing for map dispatch to props in LoginForm

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { LoginForm } from './LoginForm';
+import { LoginForm, mapDispatchToProps } from './LoginForm';
+import * as actions from '../../actions';
+
+jest.mock('../../actions');
 
 describe('LoginForm', () => {
   let wrapper;
@@ -26,5 +29,15 @@ describe('LoginForm', () => {
     wrapper.instance().handleChange(mockEvent);
 
     expect(wrapper.state('username')).toEqual('tbacher');
+  });
+
+  it('should dispatch addUser', () => {
+    const mockDispatch = jest.fn();
+    const mockUser = { username: 'fred' };
+    const mockAction = actions.addUser(mockUser);
+
+    mapDispatchToProps(mockDispatch).addUser(mockUser);
+    
+    expect(mockDispatch).toHaveBeenCalledWith(mockAction);
   });
 });
