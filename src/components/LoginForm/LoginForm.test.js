9b9faa777c0073ba0375fd46a3904cbf78e3ccbd
tiffanybacher <tiffanybacher@gmail.com>
2019-07-16 19:45:58 -0600

    Write testing for updating input for LoginForm

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -12,4 +12,19 @@ describe('LoginForm', () => {
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should update state on input change', () => {
+    expect(wrapper.state()).toEqual({
+      username: '',
+      password: ''
+    });
+
+    const mockEvent = {
+      target: { value: 'tbacher', name: 'username' }
+    };
+
+    wrapper.instance().handleChange(mockEvent);
+
+    expect(wrapper.state('username')).toEqual('tbacher');
+  });
 });
