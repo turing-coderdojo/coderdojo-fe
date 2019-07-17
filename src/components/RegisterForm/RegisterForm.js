d9ab8f784fd2df4bdee114030098a5f5305d1319
Erik Konner <erikkonner@gmail.com>
2019-07-17 10:52:55 -0600

    Created RegisterForm component and wrote snapshot test

---
+++ src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Proptypes } from 'prop-types';
+import { connect } from 'react-redux';
+
+export class RegisterForm extends Component {
+  state ={
+
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
