2992f85f155a40920545e93ce1434aeebe7e705a
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-17 11:48:38 -0600

    Create mock funcs for tests, test handleChange events for inputs

--- src/components/GuardianDetailsForm/GuardianDetailsForm.test.js
+++ src/components/GuardianDetailsForm/GuardianDetailsForm.test.js
@@ -4,12 +4,32 @@ import GuardianDetailsForm from './GuardianDetailsForm';
 
 describe('GuardianDetailsForm', () => {
   let wrapper;
+  const mockHandleSubmit = jest.fn();
+  const mockHandleChange = jest.fn();
+  const mockInputChange = {
+    target: {
+      name: 'email',
+      value: 'nim@sum.com'
+    }
+  }
 
   beforeEach(() => {
-    wrapper = shallow(<GuardianDetailsForm />);
+    wrapper = shallow(
+      <GuardianDetailsForm
+        handleSubmit={mockHandleSubmit}
+        handleChange={mockHandleChange}
+      />
+    );
   });
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should send name and value to handleChange props on input change', () => {
+    wrapper.find('input#email-input').simulate('change', mockInputChange);
+    const { name, value } = mockInputChange.target;
+    expect(mockHandleChange).toHaveBeenCalledWith(name, value);
+  });
+
 });
