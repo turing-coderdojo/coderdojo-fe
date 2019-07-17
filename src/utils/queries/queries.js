e22599d7c6446c984acb894243e70d4869b1bd54
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-17 17:00:34 -0600

    Create sign in and new student query methods

--- src/utils/queries/queries.js
+++ src/utils/queries/queries.js
@@ -2,6 +2,8 @@ import { gqlQuery, gqlMutate } from './clientQuery';
 import * as queryTypes from './queryTypes';
 
 const queries = {
+  signIn: user => gqlMutate(queryTypes.SIGN_IN, user),
+  createStudent: student => gqlMutate(queryTypes.CREATE_STUDENT, student)
 };
 
 export default queries;
