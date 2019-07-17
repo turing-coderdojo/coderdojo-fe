80f6df4e7aa84b01b33d9f03337e1b06e8640197
Erik Konner <erikkonner@gmail.com>
2019-07-17 14:06:42 -0600

    Added state testing and MDTP testing

--- src/components/RegisterForm/RegisterForm.test.js
+++ src/components/RegisterForm/RegisterForm.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { RegisterForm } from './RegisterForm';
+import { RegisterForm, mapDispatchToProps } from './RegisterForm';
+import * as actions from '../../actions';
+
+jest.mock('../../actions');
 
 describe('RegisterForm', () => {
   let wrapper;
@@ -11,5 +14,31 @@ describe('RegisterForm', () => {
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update state on input change', () => {
+    expect(wrapper.state()).toEqual({
+      fullName: '',
+      username: '',
+      password: '',
+      reEnteredPassword: ''
+    }); 
+
+    const mockEvent = {
+      target: { value: 'ehk', name: 'username' }
+    };
+
+    wrapper.instance().handleChange(mockEvent);
+    expect(wrapper.state('username')).toEqual('ehk');
+  });
+
+  it('should dispatch addUser', () => {
+    const mockDispatch = jest.fn();
+    const mockUser = { username: 'ehk' };
+    const mockAction = actions.addUser(mockUser);
+
+    mapDispatchToProps(mockDispatch).addUser(mockUser);
+
+    expect(mockDispatch).toHaveBeenCalledWith(mockAction);  
   });
 });
