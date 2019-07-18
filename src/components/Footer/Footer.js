8059ffc9285a0e678b34498db73d81490390b23d
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-18 17:45:18 -0600

    Add styling and layout to the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import globeImage from '../../images/CD-footer-globe.png';
+import fbIcon from '../../images/facebook.svg';
+import twitterIcon from '../../images/twitter.svg';
+import linkedInIcon from '../../images/linkedin.svg';
+import emailIcon from '../../images/email.svg';
 
 function Footer() {
   return (
     <footer className="Footer">
-      
       <section className="social-media">
-        s
+        <div className="icon-container">
+          <h3>Talk to our community</h3>
+          <a href="google.com">
+            <img src={fbIcon} alt="facebook" />
+            <img src={twitterIcon} alt="facebook" />
+            <img src={linkedInIcon} alt="facebook" />
+            <img src={emailIcon} alt="facebook" />
+          </a>
+        </div>
       </section>
     </footer>
   );
