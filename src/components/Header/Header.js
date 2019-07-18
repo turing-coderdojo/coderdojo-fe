05cf9472220bc87bacdc3314d3a29f77e8da1eb1
Erik Konner <erikkonner@gmail.com>
2019-07-18 13:40:34 -0600

    Styled header, finished conditional rendering of navlinks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import logo from '../../styles/images/CoderDojo.svg';
+import { ReactComponent as Logo } from '../../images/logo-black.svg';
 
 function Header(props) {
   const assignLogin = () => {
     const { user } = props;
     let loginButton;
-    if (user) {
-      loginButton = <NavLink to="/">Sign Out </NavLink>;
+    if (user.length) {
+      loginButton = <NavLink to="/" className="nav-link">Sign Out </NavLink>;
     } else {
-      loginButton = <NavLink to="/login">Login/Register</NavLink>;
+      loginButton = <NavLink to="/login" className="nav-link">Login/Register</NavLink>;
     }
 
     return loginButton;
@@ -18,9 +18,9 @@ function Header(props) {
 
   return (
     <header>
-      {logo}
+      <Logo className="logo" />
       <section>
-        <NavLink>Dojos</NavLink>
+        <NavLink className="nav-link" to="/">Dojos</NavLink>
         {assignLogin()}
       </section>
     </header>
