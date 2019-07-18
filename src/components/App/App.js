5862b4637857a361e6d05904cffa3e774287e3ae
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-17 20:14:21 -0600

    Clean App component, update snapshot test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,42 +1,15 @@
 import React, { Component } from 'react';
-import queries from '../../utils/queries/queries';
 import LoginForm from '../LoginForm/LoginForm';
 import ContactForm from '../ContactForm/ContactForm';
 import Controls from '../Controls/Controls';
 
 class App extends Component {
-  state = {
-    error: '',
-    users: [],
-  }
-
-  async componentDidMount() {
-    try {
-      const users = await queries.getAllUsers();
-
-      this.setState({ users });
-    } catch ({ message }) {
-      // this.setState({ error: message });
-    }
-  }
-  
-  createUser = async () => {
-    const mockUser = {
-      email: 'tiff@sum.com',
-      nickname: 'ERIKKKKK',
-      password: 'asdfasdf'
-    };
-    
-    await queries.newUser(mockUser);
-  }
+  state = {};
 
   render() {
     return (
       <div className="App">
         <h1>CODERDOJO!</h1>
-        <button type="submit" onClick={this.createUser}>
-          CREATE
-        </button>
         <ContactForm />
         <LoginForm />
         <Controls />
