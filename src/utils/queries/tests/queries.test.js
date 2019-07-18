680a00c9080622fed72905752e17f9d27e5a58ee
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-17 20:32:35 -0600

    Test createStudent method in queries object

--- src/utils/queries/tests/queries.test.js
+++ src/utils/queries/tests/queries.test.js
@@ -8,5 +8,27 @@ gqlQuery.mockImplementation(() => {});
 gqlMutate.mockImplementation(() => {});
 
 describe('Queries', () => {
-  
-})
+  it('signIn: should invoke mutation query with correct args', () => {
+    const user = {
+      email: 'nim',
+      password: 'sum'
+    };
+    const expectedQuery = queryTypes.SIGN_IN;
+
+    queries.signIn(user);
+    expect(gqlMutate).toHaveBeenCalledWith(expectedQuery, user);
+  });
+
+  it('createStudent: should invoke mutation query with correct args', () => {
+    const newStudent = {
+      username: 'nim',
+      password: 'sum',
+      name: 'nimsum',
+      birthdate: '07/07/7077'
+    };
+    const expectedQuery = queryTypes.CREATE_STUDENT;
+
+    queries.createStudent(newStudent);
+    expect(gqlMutate).toHaveBeenCalledWith(expectedQuery, newStudent);
+  });
+});
