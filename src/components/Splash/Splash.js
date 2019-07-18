7a9305c4759d9ca39767bc5c602df9d8f3b57a0a
tiffanybacher <tiffanybacher@gmail.com>
2019-07-18 16:13:27 -0600

    Start Splash component

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -4,7 +4,13 @@ import splash from '../../images/CD-main-banner.png';
 const Splash = () => {
   return(
     <section className="Splash">
-      <img src={splash} alt="Splash page illustration that depicts 2 children having fun at their computers."/>
+      <img 
+        src={splash} 
+        className="main-splash-img"
+        alt="Splash page illustration that depicts 2 children having fun at their computers."/>
+      <div className="green-container">
+        
+      </div>
     </section>
   );
 }
