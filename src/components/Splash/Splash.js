0130bb676e592012e5e0ac45a0593fa703fd5cf9
tiffanybacher <tiffanybacher@gmail.com>
2019-07-18 13:28:17 -0600

    Add main image to Splash component and update root Route

---
+++ src/components/Splash/Splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import splash from '../../images/CD-main-banner.png';
+
+const Splash = () => {
+  return(
+    <section className="Splash">
+      <img src={splash} alt="Splash page illustration that depicts 2 children having fun at their computers."/>
+    </section>
+  );
+}
+
+export default Splash;
