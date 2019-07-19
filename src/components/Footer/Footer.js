c4035f7528f1a40bdf6692387ffd6d203c961e41
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-18 18:20:34 -0600

    Additional footer styling and layuout changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,15 +8,17 @@ import emailIcon from '../../images/email.svg';
 function Footer() {
   return (
     <footer className="Footer">
+      <img src={globeImage} alt="globe" className="globe" />
       <section className="social-media">
         <div className="icon-container">
           <h3>Talk to our community</h3>
           <a href="google.com">
             <img src={fbIcon} alt="facebook" />
+          </a>
+
             <img src={twitterIcon} alt="facebook" />
             <img src={linkedInIcon} alt="facebook" />
             <img src={emailIcon} alt="facebook" />
-          </a>
         </div>
       </section>
     </footer>
