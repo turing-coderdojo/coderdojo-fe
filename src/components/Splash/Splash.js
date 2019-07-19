5b4cbc6755bdbd931dc149f4233a136e9705d6a9
tiffanybacher <tiffanybacher@gmail.com>
2019-07-18 19:44:57 -0600

    Create Splash component

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import splash from '../../images/CD-main-banner.png';
+import splashImg from '../../images/CD-main-banner.png';
 
-const Splash = () => {
-  return(
+function Splash() { 
+  return (
     <section className="Splash">
-      <img 
-        src={splash} 
-        className="main-splash-img"
-        alt="Splash page illustration that depicts 2 children having fun at their computers."/>
-      <div className="green-container">
-        
+      <div className="Splash-container">
+        <img 
+          src={splashImg} 
+          className="main-splash-img"
+          alt="Splash page illustration that depicts 2 children having fun at their computers." 
+        />
+        <div className="green-container">
+          
+        </div>
       </div>
     </section>
   );
