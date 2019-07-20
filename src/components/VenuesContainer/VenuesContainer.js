960ba595ae30c95c9eae8593f362da15d9ee98b8
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-19 20:49:38 -0600

    Add proptypes and default propTypes

--- src/components/VenuesContainer/VenuesContainer.js
+++ src/components/VenuesContainer/VenuesContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import VenueSearchBar from '../VenueSearchBar/VenueSearchBar';
 
 function VenuesContainer({ venues, isLoading, error }) {
@@ -28,5 +29,17 @@ const mapStateToProps = state => ({
   isLoading: state.isFetching,
   error: state.error
 });
+
+VenuesContainer.propTypes = {
+  venues: PropTypes.array,
+  isLoading: PropTypes.bool,
+  error: PropTypes.string
+};
+
+VenuesContainer.defaultProps = {
+  venues: [],
+  isLoading: false,
+  error: ''
+};
 
 export default connect(mapStateToProps)(VenuesContainer);
