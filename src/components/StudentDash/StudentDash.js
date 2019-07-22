20bf50f3870ca9fa869812ea8a5422af24190848
Erik Konner <erikkonner@gmail.com>
2019-07-22 16:55:21 -0600

    Set up PropTypes, have input and submit for event code

--- src/components/StudentDash/StudentDash.js
+++ src/components/StudentDash/StudentDash.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import requests from '../../utils/requests/requests';
 
-export function StudentDash() {
+
+export function StudentDash(props) {
+  // const [user, setUser] = useState({});
+  const { user } = props;
+
   return (
-    <section>
-
+    <section className="StudentDashContainer">
+      <div className="StudentDashHeader">
+        <h2>
+          Welcome, {user.name}
+        </h2>
+        
+      </div>
+      <form>
+        <input 
+          type="text" 
+          placeholder="Enter Event Code"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </section>
   );
 }
 
-export default StudentDash;
+export const mapStateToProps = ({ user, isFetching, error }) => ({
+  user,
+  isFetching,
+  error
+});
+
+export default connect(mapStateToProps, null)(StudentDash);
+
+StudentDash.propTypes = {
+  user: PropTypes.object,
+  isFetching: PropTypes.bool,
+  error: PropTypes.string
+};
+
+StudentDash.defaultProps = {
+  user: {},
+  isFetching: false,
+  error: ''
+};
