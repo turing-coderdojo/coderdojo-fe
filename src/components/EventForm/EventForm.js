c74b807e48d8fdc45126a22a25df4fda590dfe8f
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-22 20:56:44 -0600

    Create inputs for event name and notes, create on change handler for inputs

--- src/components/EventForm/EventForm.js
+++ src/components/EventForm/EventForm.js
@@ -1,10 +1,40 @@
 import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
 
 function EventForm() {
+  const [eventDetails, setEventDetails] = useState({});
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setEventDetails({ ...eventDetails, [name]: value });
+  };
+
   return (
-    <form className="EventForm">
-      Event Form
-    </form>
+    <div className="EventForm">
+      <form>
+        <h2>Create New Event:</h2>
+        <label htmlFor="event-name">
+          Event Name:
+          <input
+            id="event-name"
+            type="text"
+            name="name"
+            onChange={handleChange}
+          />
+        </label>
+        
+        <label htmlFor="event-notes">
+          Notes(Optional):
+          <textarea
+            id="event-notes"
+            type="text"
+            name="notes"
+            onChange={handleChange}
+          />
+        </label>
+        <button type="button" className="create-event-btn" name="student" onClick={() => {}}>Create</button>
+      </form>
+    </div>
   );
 }
 
