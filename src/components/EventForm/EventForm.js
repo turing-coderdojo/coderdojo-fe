3e9c044ba7ab66bbb0b6a0e27ee198c3738a3e9e
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-22 21:45:03 -0600

    Create inputs for start and end hours

--- src/components/EventForm/EventForm.js
+++ src/components/EventForm/EventForm.js
@@ -3,10 +3,23 @@ import DatePicker from 'react-datepicker';
 
 function EventForm() {
   const [eventDetails, setEventDetails] = useState({});
+  const [startEndDate, setStartEndDate] = useState({});
 
   const handleChange = ({ target }) => {
     const { name, value } = target;
     setEventDetails({ ...eventDetails, [name]: value });
+  };
+
+  const handleDateChange = (date, isEndTime) => {
+    const startTime = new Date(date).toUTCString();
+    const endTime = new Date(date).toUTCString();
+    if (isEndTime) {
+      setEventDetails({ ...eventDetails, end_time: endTime });
+      setStartEndDate({ ...startEndDate, end: date });
+    } else {
+      setEventDetails({ ...eventDetails, start_time: startTime });
+      setStartEndDate({ ...startEndDate, start: date });
+    }
   };
 
   return (
@@ -22,7 +35,22 @@ function EventForm() {
             onChange={handleChange}
           />
         </label>
-        
+        <DatePicker
+          selected={startEndDate.start}
+          onChange={date => handleDateChange(date, false)}
+          minDate={new Date()}
+          showTimeSelect
+          dateFormat="MMMM d, yyyy h:mm aa"
+          placeholderText="Start Date & Time"
+        />
+        <DatePicker
+          selected={startEndDate.end}
+          onChange={date => handleDateChange(date, true)}
+          minDate={new Date()}
+          showTimeSelect
+          dateFormat="MMMM d, yyyy h:mm aa"
+          placeholderText="Start Date & Time"
+        />
         <label htmlFor="event-notes">
           Notes(Optional):
           <textarea
