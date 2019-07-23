595c6cbd7c7a00bd9dd69901fb198b839bce89ed
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-22 18:57:59 -0600

    Conditionally render event cards

--- src/components/AdminDash/AdminDash.js
+++ src/components/AdminDash/AdminDash.js
@@ -5,15 +5,18 @@ import requests from '../../utils/requests/requests';
 import EventCard from '../EventCard/EventCard';
 
 export function AdminDash(props) {
-  const [venuesAndEvents, setVenuesAndEvents] = useState([]);
+  const [venuesAndEvents, setVenuesAndEvents] = useState({});
   const { user } = props;
-
+  
   const getEventsAndVenues = async () => {
     const result = await requests.getAdminDetails();
     const { venues } = result.me;
-    setVenuesAndEvents(...venues);
+    setVenuesAndEvents(venues[0]);
   };
 
+  const generateEventCard = () => {
+    return venuesAndEvents.events.map(event => <EventCard event={event} key={event.id} />);
+  };
   
   useEffect(() => {
     getEventsAndVenues();
@@ -29,10 +32,13 @@ export function AdminDash(props) {
       </div>
       <div className="details-container">
         <section className="venue-section">
-          venueww
         </section>
         <section className="events-section">
-          eventss
+          <div className="events-header">
+            <p>Your Upcoming Events:</p>
+            <button type="button">+ Create New Event</button>
+          </div>
+          {venuesAndEvents.events && generateEventCard()}
         </section>
       </div>
     </section>
