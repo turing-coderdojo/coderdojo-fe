eeb611187fb7a27e2c37cc5e08abf8b9623bc2e7
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-24 20:30:43 -0600

    Conditionally render student guardian on hover

--- src/components/StudentAttendanceCard/StudentAttendanceCard.js
+++ src/components/StudentAttendanceCard/StudentAttendanceCard.js
@@ -1,32 +1,40 @@
 import React, { useState } from 'react';
 
 function StudentAttendanceCard({ student }) {
+  const [guardianInfo, showGuardianInfo] = useState(false);
   const { 
     name, username, email, phoneNumber 
   } = student.guardianId;
+  const guardianInfoBox = (
+    <div className="guardian-details">
+      <p>Guardian Details:</p>
+      <p>
+        {name}
+        :
+        {username}
+      </p>
+      <p>
+        Phone:
+        {phoneNumber}
+      </p>
+      <p>
+        Email:
+        {email}
+      </p>
+    </div>
+  );
   return (
-    <article key={student.id}>
+    <article 
+      onMouseEnter={() => showGuardianInfo(true)} 
+      onMouseLeave={() => showGuardianInfo(false)} 
+      key={student.id}
+    >
       <h4>
         {student.name}
         :
         {student.username}
       </h4>
-      <div className="guardian-details">
-        <h4>Guardian Details:</h4>
-        <p>
-          {name}
-          :
-          {username}
-        </p>
-        <p>
-          Phone:
-          {phoneNumber}
-        </p>
-        <p>
-          Email:
-          {email}
-        </p>
-      </div>
+      {guardianInfo && guardianInfoBox}
     </article>
   );
 }
