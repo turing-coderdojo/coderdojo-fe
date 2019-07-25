107704c44a3f1bae359c23620c284c0dd1ee2876
tiffanybacher <tiffanybacher@gmail.com>
2019-07-24 21:58:03 -0600

    Test ContactForm

--- src/components/ContactForm/ContactForm.test.js
+++ src/components/ContactForm/ContactForm.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { ContactForm } from './ContactForm';
+import { ContactForm, mapStateToProps, mapDispatchToProps } from './ContactForm';
+import * as actions from '../../actions';
 
 describe('ContactForm', () => {
   let wrapper;
@@ -54,5 +55,80 @@ describe('ContactForm', () => {
   it('should invoke handleSubmit prop on form submit', () => {
     wrapper.find('form.ContactForm').simulate('submit', mockSubmitEvent);
     expect(mockSubmitEvent.preventDefault).toHaveBeenCalled();
-  });    
+  });
+
+  it('should checkAllFields and return false if all fields are full', () => {
+    wrapper.setState({
+      email: 'tiffanybacher@gmail.com',
+      phoneNumber: '1234567890',
+      street1: '123 address',
+      city: 'Denver',
+      state: 'CO',
+      zip: '80203'
+    });
+
+    const result = wrapper.instance().checkAllFields();
+
+    expect(result).toEqual(false);
+  }); 
+
+  it('should check all fields and return true if all fields are not full', () => {
+    wrapper.setState({
+      email: 'tiffanybacher@gmail.com',
+      phoneNumber: '',
+      street1: '123 address',
+      city: 'Denver',
+      state: 'CO',
+      zip: '80203'
+    });
+
+    const result = wrapper.instance().checkAllFields();
+
+    expect(result).toEqual(true);
+  });
+
+  describe('mdtp', () => {
+    let mockDispatch;
+
+    beforeEach(() => {
+      mockDispatch = jest.fn();
+    });
+
+    it('should dispatch addUser', () => {
+      const user = { id: 1, username: 'tiff' };
+      const action = actions.addUser(user);
+
+      mapDispatchToProps(mockDispatch).addUser(user);
+
+      expect(mockDispatch).toHaveBeenCalledWith(action);
+    });
+
+    it('should dispatch setError', () => {
+      const error = 'There was an error';
+      const action = actions.setError(error);
+
+      mapDispatchToProps(mockDispatch).setError(error);
+
+      expect(mockDispatch).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe('mstp', () => {
+    const state = {
+      error: 'There was an error',
+      isFetching: false
+    };
+
+    it('should map error to props', () => {
+      const prop = mapStateToProps(state).error;
+
+      expect(prop).toEqual(state.error);
+    });
+
+    it('should map isFetching to props', () => {
+      const prop = mapStateToProps(state).isFetching;
+
+      expect(prop).toEqual(state.isFetching);
+    });
+  });
 });
--- src/reducers/studentsReducer.test.js
+++ src/reducers/studentsReducer.test.js
@@ -1,7 +1,23 @@
 import studentsReducer from './studentsReducer';
+import * as actions from '../actions';
 
 describe('studentsReducer', () => {
-  it('should return default state', () => {
+  const students = [
+    { id: 1, username: 'tiff' },
+    { id: 2, username: 'jjb' }
+  ];
 
+  it('should return default state', () => {
+    const expected = [];
+    const result = studentsReducer(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should add students to state tree', () => {
+    const action = actions.addStudents(students);
+    const result = studentsReducer([], action);
+
+    expect(result).toEqual(students);
   });
 });
